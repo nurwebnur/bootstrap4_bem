958bc37fda665717ce0620de8f4d3623a95b319f
nurwebnur <nur.web.nur@gmail.com>
2019-03-20 17:50:26 +0600

    upd portfolio

--- js/main.js
+++ js/main.js
@@ -4,11 +4,16 @@ $(document).ready(function () {
             console.log("one");
             $(".filter > div*[filter !='" + $(this).attr('filter') + "']").fadeOut(1350);
             $(".filter > div*[filter ='" + $(this).attr('filter') + "']").fadeIn(1350);
+            $('.button[filter]').removeClass('btn_active');
+            $(this).addClass('btn_active');
+            console.log($(this).attr('filter'));
             // $(".filter > div*[filter !='" + $(this).attr('filter') + "']").hide(350);
             // $(".filter > div*[filter ='" + $(this).attr('filter') + "']").show(350);
         } else {
             console.log("all");
             $(".filter > div").fadeIn(1350);
+            $('.button[filter]').removeClass('btn_active');
+            $(this).addClass('btn_active');
             // $(".filter > div").show(350);
         }
     });
